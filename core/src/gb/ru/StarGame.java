8f9cad859cc213de0d260a0a96fe2e32a2c110db
Daniil <saponembigas@gmail.com>
2021-10-21 17:54:42 +0300

    Lesson-2

--- core/src/gb/ru/StarGame.java
+++ core/src/gb/ru/StarGame.java
@@ -1,31 +1,15 @@
 package gb.ru;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
 
-public class StarGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+import com.badlogic.gdx.Game;
+
+import gb.ru.screen.MenuScreen;
+
+public class StarGame extends Game {
+
 	
 	@Override
 	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("space.jpg");
-	}
-
-	@Override
-	public void render () {
-		ScreenUtils.clear(1, 0, 0, 1);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
-	}
-	
-	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
-	}
+		setScreen(new MenuScreen());
+	};
 }
